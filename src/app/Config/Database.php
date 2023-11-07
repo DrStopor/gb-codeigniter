43756ea7422294eb8e3f4fdb7283000fe72bf299
d.rudov <rds18@mail.ru>
2023-11-08 01:46:53 +0300

    Добавлен докер

--- src/app/Config/Database.php
+++ src/app/Config/Database.php
@@ -26,10 +26,10 @@ class Database extends Config
      */
     public array $default = [
         'DSN'          => '',
-        'hostname'     => 'localhost',
-        'username'     => '',
-        'password'     => '',
-        'database'     => '',
+        'hostname'     => '127.0.0.1',
+        'username'     => 'root',
+        'password'     => 'cod31gn1t3',
+        'database'     => 'codeigniter_db',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
