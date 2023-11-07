dbe75ba81e142daf054624620906b7bd0fe139d3
d.rudov <rds18@mail.ru>
2023-11-08 01:46:53 +0300

    Добавлен докер

--- src/app/Config/Routes.php
+++ src/app/Config/Routes.php
@@ -5,7 +5,7 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'MessagesController::partList');
+$routes->get('/', 'MessagesController::partList/3/id/asc');
 $routes->get('/(:alpha)/(:alpha)', 'MessagesController::partList/3/$1/$2');
 $routes->get('/message/delete/(:num)', 'MessagesController::delete/$1');
 $routes->post('/message/create', 'MessagesController::create');
